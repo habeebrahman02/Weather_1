215b825f685e308e74a447c8d479958941744973
HABEEB RAHMAN J <127578663+habeebrahman02@users.noreply.github.com>
2025-05-20 16:02:19 +0530

    Add files via upload

---
+++ Weather.jsx
@@ -0,0 +1,51 @@
+// src/Weather.js
+
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Weather = () => {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleSearch = async () => {
+    const apiKey = '1b47dd1ff23131be62104b02a72fed67';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+      const response = await axios.get(url);
+      setWeatherData(response.data);
+      setError(null);
+    } catch (err) {
+      setWeatherData(null);
+      setError('City not found or API error.');
+    }
+  };
+
+  return (
+    <div className="weather-container">
+      <h1>Weather App</h1>
+      <input
+        type="text"
+        placeholder="Enter city"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      /> <br /><br />
+      <button onClick={handleSearch}>Get Weather</button>
+
+      {error && <p>{error}</p>}
+
+      {weatherData && (
+        <div>
+          <h2>{weatherData.name}</h2>
+          <p>{weatherData.weather[0].description}</p>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Humidity: {weatherData.main.humidity}%</p>
+          <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Weather;
